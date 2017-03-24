ddce3cfe02436362d4244c83b7318f93d39636eb
jeremyeytler <jeremy.e.tyler@gmail.com>
2017-03-24 13:10:12 -0400

    Creating extremely basic game template, Updating README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pypong
 A PyGame Implementation of Pong.
+
+Trying to implement it as closely as possible to the original:
+https://www.youtube.com/watch?v=fiShX2pTz9A
--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
-def hello():
-    print "Hello, World!"
+"""Basic pygame setup."""
+import sys
+import pygame
 
+SIZE = WIDTH, HEIGHT = (320, 240)
 
-hello()
+screen = pygame.display.set_mode(SIZE)
+
+# Main Game Loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        print event.event_name(event.type)
